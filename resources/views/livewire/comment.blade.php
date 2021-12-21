aa2dc364c558bcfe2b154ae27e3c65c0e8880c52
freek <freek@spatie.be>
2021-12-21 17:33:52 +0100

    wip

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -1,18 +1,23 @@
-<div id="comment{{ $comment->id }}" @class(["bg-white border-b border-gray-300 pb-8"=> $comment->isTopLevel()])>
-
-    <div class=' flex p-4 pb-0 group rounded-md' ])>
+<div
+    id="comment{{ $comment->id }}"
+    @class(["bg-white border-b border-gray-300 pb-8"=> $comment->isTopLevel()])
+>
+    <div class="flex p-4 pb-0 group rounded-md" )>
         <div @class(["flex-shrink-0 mr-4", "mt-4"=> !$comment->isTopLevel()])>
             @include('comments::livewire.partials.avatar')
         </div>
 
         <div @class(['flex-grow', 'border p-4 rounded-md border-gray-300'=> !$comment->isTopLevel()])>
-
             @include('comments::livewire.partials.commentHeader')
 
             <div class="mt-1 flex-grow w-full markdown @if($comment->isTopLevel()) toplevel-markdown @endif">
                 @if ($isEditing)
-                    <livewire:comments-compose :on-submit="'edit:' . $comment->id" :on-cancel="'cancel:' . $comment->id"
-                                               :text="$comment->original_text" :primaryColor="$primaryColor"/>
+                    <livewire:comments-compose
+                        :on-submit="'edit:' . $comment->id"
+                        :on-cancel="'cancel:' . $comment->id"
+                        :text="$comment->original_text"
+                        :primaryColor="$primaryColor"
+                    />
                 @else
                     <p class=" text-gray-700">{!! $comment->text !!}</p>
                 @endif
@@ -24,14 +29,20 @@
 
     <div class="ml-[4.5rem] mt-6 relative">
         @foreach ($comment->nestedComments as $nestedComment)
-            <livewire:comments-comment :comment="$nestedComment" :key="$nestedComment->id"
-                                       :primaryColor="$primaryColor"/>
+            <livewire:comments-comment
+                :comment="$nestedComment"
+                :key="$nestedComment->id"
+                :primaryColor="$primaryColor"
+            />
         @endforeach
 
         <div id="reply-form-{{ $comment->id }}">
             @if ($isReplying)
-                <livewire:comments-compose :on-submit="'reply:'  . $comment->id" :on-cancel="'cancel:' . $comment->id"
-                                           :primaryColor="$primaryColor"/>
+                <livewire:comments-compose
+                    :on-submit="'reply:'  . $comment->id"
+                    :on-cancel="'cancel:' . $comment->id"
+                    :primaryColor="$primaryColor"
+                />
             @endif
         </div>
 
@@ -39,13 +50,15 @@
         @if (!$isReplying)
             @if ($comment->isTopLevel())
                 @auth
-                    <div class=" flex border border-gray-300 p-4 rounded-md">
+                    <div class="flex border border-gray-300 p-4 rounded-md">
                         <div class="flex-shrink-0 mr-4">
                             @include('comments::livewire.partials.avatar')
                         </div>
-                        <input wire:click="$toggle('isReplying')" class="w-full border border-gray-300 rounded-md px-4"
-                               type="text"
-                               placeholder="Write reply">
+                        <input
+                            wire:click="$toggle('isReplying')" class="w-full border border-gray-300 rounded-md px-4"
+                            type="text"
+                            placeholder="Write reply"
+                        />
                     </div>
                 @endauth
             @endif
--- src/LivewireCommentsServiceProvider.php
+++ src/LivewireCommentsServiceProvider.php
@@ -17,6 +17,7 @@ class LivewireCommentsServiceProvider extends PackageServiceProvider
     {
         $package
             ->name('laravel-comments-livewire')
+            ->hasTranslations()
             ->hasViews('comments');
     }
 
