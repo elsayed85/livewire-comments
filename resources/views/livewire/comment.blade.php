9f5d29cfce19fe296bd0c154816bff56dea2da89
freek <freek@spatie.be>
2021-12-21 16:58:56 +0100

    wip

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -5,7 +5,7 @@
 
         <div @class(["flex-shrink-0 mr-4", "mt-4"=> !$comment->isTopLevel()])>
             <img class="h-10 w-10 rounded-full" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
-                alt="{{ $comment->user->name }}">
+                 alt="{{ $comment->user->name }}">
         </div>
 
         <div @class(['flex-grow', 'border p-4 rounded-md border-gray-300'=> !$comment->isTopLevel()])>
@@ -14,64 +14,64 @@
 
             <div class="mt-1 flex-grow w-full markdown @if($comment->isTopLevel()) toplevel-markdown @endif">
                 @if ($isEditing)
-                <livewire:comments-compose :on-submit="'edit:' . $comment->id" :on-cancel="'cancel:' . $comment->id"
-                    :text="$comment->original_text" :primaryColor="$primaryColor" />
+                    <livewire:comments-compose :on-submit="'edit:' . $comment->id" :on-cancel="'cancel:' . $comment->id"
+                                               :text="$comment->original_text" :primaryColor="$primaryColor"/>
                 @else
-                <p class=" text-gray-700">{!! $comment->text !!}</p>
+                    <p class=" text-gray-700">{!! $comment->text !!}</p>
                 @endif
             </div>
 
             <div class="my-4 relative  flex items-center space-x-4">
 
                 @foreach($comment->reactions->summary() as $summary)
-                <div wire:click="deleteReaction('{{ $summary['reaction'] }}')"
-                    class="rounded-full cursor-pointer hover:bg-gray-300 bg-gray-200 py-1 px-2 text-sm @if($summary['user_reacted']) border border-{!! $primaryColor !!} bg-{!! $primaryColor !!} bg-opacity-25 hover:bg-opacity-50 @endif">
-                    {{ $summary['reaction'] }} {{ $summary['count'] }}
-                </div>
+                    <div wire:click="deleteReaction('{{ $summary['reaction'] }}')"
+                         class="rounded-full cursor-pointer hover:bg-gray-300 bg-gray-200 py-1 px-2 text-sm @if($summary['user_reacted']) border border-{!! $primaryColor !!} bg-{!! $primaryColor !!} bg-opacity-25 hover:bg-opacity-50 @endif">
+                        {{ $summary['reaction'] }} {{ $summary['count'] }}
+                    </div>
                 @endforeach
 
 
-
-
-                <div x-cloak class="p-1 z-30 relative cursor-pointer rounded-md hover:bg-gray-200" x-data="{ open: false }"
-                    x-on:click.outside="open = false">
+                <div x-cloak class="p-1 z-30 relative cursor-pointer rounded-md hover:bg-gray-200"
+                     x-data="{ open: false }"
+                     x-on:click.outside="open = false">
                     @auth
-                    <svg x-on:click="open = !open" xmlns="http://www.w3.org/2000/svg" class="h-5  w-5 stroke-gray-700"
-                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <div x-show="open"
-                        class="absolute  bg-white shadow-lg  rounded-md left-full top-0 transform translate-x-4">
+                        <svg x-on:click="open = !open" xmlns="http://www.w3.org/2000/svg"
+                             class="h-5  w-5 stroke-gray-700"
+                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+                        <div x-show="open"
+                             class="absolute  bg-white shadow-lg  rounded-md left-full top-0 transform translate-x-4">
 
 
-                        @can('react', $comment)
-                        <div x-on:click="open = !open" class=" px-1 py-1  grid gap-1 grid-cols-5 w-56  ">
+                            @can('react', $comment)
+                                <div x-on:click="open = !open" class=" px-1 py-1  grid gap-1 grid-cols-5 w-56  ">
 
-                            @foreach(config('comments.allowed_reactions') as $reaction)
+                                    @foreach(config('comments.allowed_reactions') as $reaction)
 
 
-                            @php
-                                $userReacted = !is_bool(array_search($reaction, array_column($comment->reactions()->get()->toArray(), 'reaction')))
-                            @endphp
+                                        @php
+                                            $userReacted = !is_bool(array_search($reaction, array_column($comment->reactions()->get()->toArray(), 'reaction')))
+                                        @endphp
 
-                            <div class="border-1 text-center col-span-1 hover:bg-gray-100 p-2 rounded-md cursor-pointer  @if($userReacted) bg-{!! $primaryColor !!} hover:bg-{!! $primaryColor !!} bg-opacity-25 hover:bg-opacity-50 @endif"
-                                @if($userReacted)
-                                    wire:click="deleteReaction('{{ $reaction }}')"
-                                @else
-                                    wire:click="react('{{ $reaction }}')"
-                                @endif
-                                >
-                                {{ $reaction }}
+                                        <div
+                                            class="border-1 text-center col-span-1 hover:bg-gray-100 p-2 rounded-md cursor-pointer  @if($userReacted) bg-{!! $primaryColor !!} hover:bg-{!! $primaryColor !!} bg-opacity-25 hover:bg-opacity-50 @endif"
+                                            @if($userReacted)
+                                            wire:click="deleteReaction('{{ $reaction }}')"
+                                            @else
+                                            wire:click="react('{{ $reaction }}')"
+                                            @endif
+                                        >
+                                            {{ $reaction }}
+                                        </div>
+                                    @endforeach
                                 </div>
-                            @endforeach
-                        </div>
-                        @endcan
+                            @endcan
 
-                    </div>
+                        </div>
                     @endauth
                 </div>
-
 
 
             </div>
@@ -82,31 +82,33 @@
 
     <div class="ml-[4.5rem] mt-6 relative">
         @foreach ($comment->nestedComments as $nestedComment)
-        <livewire:comments-comment :comment="$nestedComment" :key="$nestedComment->id" :primaryColor="$primaryColor" />
+            <livewire:comments-comment :comment="$nestedComment" :key="$nestedComment->id"
+                                       :primaryColor="$primaryColor"/>
         @endforeach
 
         <div id="reply-form-{{ $comment->id }}">
             @if ($isReplying)
-            <livewire:comments-compose :on-submit="'reply:'  . $comment->id" :on-cancel="'cancel:' . $comment->id"
-                :primaryColor="$primaryColor" />
+                <livewire:comments-compose :on-submit="'reply:'  . $comment->id" :on-cancel="'cancel:' . $comment->id"
+                                           :primaryColor="$primaryColor"/>
             @endif
         </div>
 
 
         @if (!$isReplying)
-        @if ($comment->isTopLevel())
-        @auth
-        <div class=" flex border border-gray-300 p-4 rounded-md">
-            <div class="flex-shrink-0 mr-4">
-                <img class="h-10 w-10 rounded-full"
-                    src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
-                    alt="{{ $comment->user->name }}">
-            </div>
-            <input wire:click="$toggle('isReplying')" class="w-full border border-gray-300 rounded-md px-4" type="text"
-                placeholder="write reply">
-        </div>
-        @endauth
-        @endif
+            @if ($comment->isTopLevel())
+                @auth
+                    <div class=" flex border border-gray-300 p-4 rounded-md">
+                        <div class="flex-shrink-0 mr-4">
+                            <img class="h-10 w-10 rounded-full"
+                                 src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+                                 alt="{{ $comment->user->name }}">
+                        </div>
+                        <input wire:click="$toggle('isReplying')" class="w-full border border-gray-300 rounded-md px-4"
+                               type="text"
+                               placeholder="Write reply">
+                    </div>
+                @endauth
+            @endif
         @endif
     </div>
 </div>
