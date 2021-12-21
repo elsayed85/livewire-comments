77a388076266703f73754b63b699c9a1ccc23ff2
freek <freek@spatie.be>
2021-12-21 16:59:52 +0100

    wip

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,5 +1,5 @@
-<section >
-    
+<section>
+
 
     <div class="">
         <div class="divide-y divide-gray-200">
@@ -9,40 +9,38 @@
             <div class=" py-6">
                 <div class="space-y-8">
                     @if ($comments->count())
-                    @foreach($comments as $comment)
-                    <livewire:comments-comment :key="$comment->id" :comment="$comment" :primaryColor="$primaryColor" />
-                    @endforeach
+                        @foreach($comments as $comment)
+                            <livewire:comments-comment :key="$comment->id" :comment="$comment"
+                                                       :primaryColor="$primaryColor"/>
+                        @endforeach
 
-                    {{ $comments->links() }}
+                        {{ $comments->links() }}
                     @else
-                    <p>No comments yet</p>
+                        <p>No comments yet</p>
                     @endif
                 </div>
             </div>
         </div>
 
-
         @can('createComment', $model)
-        <div class="bg-gray-50 px-4 py-6 sm:px-6">
-            @auth
-            <div class="flex">
-                <div class="flex-shrink-0 mr-4">
-                    <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
-                        alt="{{ auth()->user()->name }}" class="h-10 w-10 rounded-full">
-                </div>
-                <div class="min-w-0 flex-1">
-                    <livewire:comments-compose :on-submit="'comment:' . $model->id" :primaryColor="$primaryColor" />
-                </div>
-            </div>
-            @endauth
+            <div class="bg-gray-50 px-4 py-6 sm:px-6">
+                @auth
+                    <div class="flex">
+                        <div class="flex-shrink-0 mr-4">
+                            <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+                                 alt="{{ auth()->user()->name }}" class="h-10 w-10 rounded-full">
+                        </div>
+                        <div class="min-w-0 flex-1">
+                            <livewire:comments-compose :on-submit="'comment:' . $model->id"
+                                                       :primaryColor="$primaryColor"/>
+                        </div>
+                    </div>
+                @endauth
 
-            @guest
-            <p>Log in to comment</p>
-            @endguest
-        </div>
+                @guest
+                    <p>Log in to comment</p>
+                @endguest
+            </div>
         @endcan
     </div>
-
-
-    
 </section>
