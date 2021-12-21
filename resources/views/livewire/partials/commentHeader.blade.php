ff7373df22edc37a1b5c428a612428b3172f7f27
freek <freek@spatie.be>
2021-12-21 16:56:44 +0100

    wip

---
+++ resources/views/livewire/partials/commentHeader.blade.php
@@ -0,0 +1,79 @@
+<div class="flex gap-4 relative z-50 justify-between">
+    <div class="flex gap-4 items-center">
+        <a href="#" class="font-medium text-gray-900">{{ $comment->user->name }}</a>
+        <div class="bg-gray-400 w-1 h-1 rounded-full"></div>
+        <span class="text-gray-400 ">
+                        {{ $comment->created_at->diffForHumans() }}
+                    </span>
+    </div>
+    <div x-cloak class="flex-grow gap-4 relative items-center" x-data="{ menuOpen: false, deleteConfirm: false }"
+         x-on:click.outside="menuOpen = deleteConfirm = false">
+        @unless($isEditing)
+
+
+            <button type="button" x-on:click="menuOpen = !menuOpen"
+                    class="flex items-center float-right gap-2 hover:bg-gray-200  rounded-md cursor-pointer px-2 py-1">
+                <svg x-show="!menuOpen" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5  " fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                </svg>
+                <svg x-show="menuOpen" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            <div class="absolute w-48 bg-white rounded-md shadow-xl right-12">
+                @can('update', $comment)
+                    <div x-show="menuOpen"
+                         class="p-4 flex items-center gap-2  text-sm cursor-pointer rounded-md hover:bg-gray-200"
+                         wire:click="startEditing">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5  w-5 stroke-gray-700" fill="none"
+                             viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                        Edit
+                    </div>
+                @endcan
+                @can('delete', $comment)
+                    <div x-show="menuOpen"
+                         class="p-4 flex gap-2 items-center  text-sm relative cursor-pointer rounded-md hover:bg-gray-200"
+                         x-on:click.outside="deleteConfirm = false" x-on:click="deleteConfirm = !deleteConfirm">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5  w-5 stroke-gray-700" fill="none"
+                             viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                        Delete
+                        <div x-show="deleteConfirm"
+                             class="absolute z-40 w-64 bg-white shadow-lg mt-4 rounded-md top-0 transform translate-y-4 right-0">
+                            <div
+                                class="w-5  inline-block absolute right-0 transform -translate-x-1/4 -translate-y-full">
+                                <div class=" h-3 w-3 bg-white rotate-45 rounded-tl-md transform origin-bottom-left">
+                                </div>
+                            </div>
+                            <div class=" normal-case p-4 ">
+                                <p class="font-bold text-lg">Delete Comment</p>
+                                <p class="mb-4">Are you sure? This can not be undone.</p>
+                                <button type="button"
+                                        class="text-white flex items-center float-right mb-2 gap-2  rounded-md bg-rose-600 px-4 py-2"
+                                        wire:click="deleteComment">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5  w-5 stroke-wite" fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                @endcan
+
+            </div>
+        @endunless
+    </div>
+</div>
