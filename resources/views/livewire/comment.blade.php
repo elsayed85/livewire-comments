ec5cfa89a455b4fc70a9824b8f27a8c479f958b2
vicdenys <vicdenys@hotmail.com>
2021-12-21 15:23:42 +0100

    wip

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -38,19 +38,19 @@
                     <div class="absolute w-48 bg-white rounded-md shadow-xl right-12">
                         @can('update', $comment)
                         <div x-show="menuOpen"
-                            class="p-4 flex items-center gap-2 uppercase text-sm cursor-pointer rounded-md hover:bg-gray-200"
+                            class="p-4 flex items-center gap-2  text-sm cursor-pointer rounded-md hover:bg-gray-200"
                             wire:click="startEditing">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5  w-5 stroke-gray-700" fill="none"
                                 viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                             </svg>
-                            edit
+                            Edit
                         </div>
                         @endcan
                         @can('delete', $comment)
                         <div x-show="menuOpen"
-                            class="p-4 flex gap-2 items-center uppercase text-sm relative cursor-pointer rounded-md hover:bg-gray-200"
+                            class="p-4 flex gap-2 items-center  text-sm relative cursor-pointer rounded-md hover:bg-gray-200"
                             x-on:click.outside="deleteConfirm = false" x-on:click="deleteConfirm = !deleteConfirm">
 
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5  w-5 stroke-gray-700" fill="none"
@@ -58,7 +58,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
-                            delete
+                            Delete
                             <div x-show="deleteConfirm"
                                 class="absolute z-50 w-64 bg-white shadow-lg mt-4 rounded-md top-0 transform translate-y-4 right-0">
                                 <div
