f7c3d4be68fbfe62f398abf7bd192611ce396e02
vicdenys <vicdenys@hotmail.com>
2021-12-21 11:34:33 +0100

    wip

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -95,7 +95,7 @@
 
                 @foreach($comment->reactions->summary() as $summary)
                 <div wire:click="deleteReaction('{{ $summary['reaction'] }}')"
-                    class="rounded-full cursor-pointer hover:bg-gray-300 bg-gray-200 py-1 px-2 text-sm @if($summary['user_reacted']) border border-[{!! $primaryColor !!}] bg-[{!! $primaryColor !!}] bg-opacity-25 hover:bg-opacity-50 @endif">
+                    class="rounded-full cursor-pointer hover:bg-gray-300 bg-gray-200 py-1 px-2 text-sm @if($summary['user_reacted']) border border-{!! $primaryColor !!} bg-{!! $primaryColor !!} bg-opacity-25 hover:bg-opacity-50 @endif">
                     {{ $summary['reaction'] }} {{ $summary['count'] }}
                 </div>
                 @endforeach
--- resources/views/livewire/compose.blade.php
+++ resources/views/livewire/compose.blade.php
@@ -10,7 +10,7 @@
     @enderror
     <div class="mt-3 flex items-center space-x-4">
         <button type="submit"
-            class="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-[{{ $primaryColor }}] hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            class="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-{{ $primaryColor }} hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
             Comment
             
