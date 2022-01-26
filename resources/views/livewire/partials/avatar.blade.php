4cec17857d599a34fffba2ce5ac0ea17b25e9122
freek <freek@spatie.be>
2022-01-26 12:10:47 +0100

    wip

--- resources/views/livewire/partials/avatar.blade.php
+++ resources/views/livewire/partials/avatar.blade.php
@@ -1,5 +1,5 @@
 <img
     class="h-10 w-10 rounded -full"
     src="{{ isset($comment) ? $comment->commentatorProperties()->avatar : 'default.png' }}"
-    alt="{{ isset($comment) ? $comment->commentatorProperties()->name : auth()->user()->commentatorProperties()->name }}"
+    alt="avatar"
 >
