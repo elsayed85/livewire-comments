412bb103bbdc8f834acdd90f54785e92f0e7f67c
freekmurze <freekmurze@users.noreply.github.com>
2022-03-21 16:30:21 +0000

    Fix styling

--- tests/Livewire/CommentComponentTest.php
+++ tests/Livewire/CommentComponentTest.php
@@ -4,7 +4,7 @@ use Livewire\Livewire;
 use Spatie\Comments\Models\Comment;
 use Spatie\LivewireComments\Livewire\CommentComponent;
 
-it('can mount the render the comment component', function() {
+it('can mount the render the comment component', function () {
     $comment = Comment::factory()->create();
 
     Livewire::test(CommentComponent::class, ['comment' => $comment])->assertSuccessful();
--- tests/Livewire/CommentsComponentTest.php
+++ tests/Livewire/CommentsComponentTest.php
@@ -2,17 +2,16 @@
 
 use Livewire\Livewire;
 use Spatie\Comments\Models\Comment;
-use Spatie\LivewireComments\Livewire\CommentComponent;
 use Spatie\LivewireComments\Livewire\CommentsComponent;
 use Spatie\LivewireComments\Tests\Support\Models\Post;
 
-it('can mount the render the comments component for a model without comments', function() {
+it('can mount the render the comments component for a model without comments', function () {
     $post = Post::factory()->create();
 
     Livewire::test(CommentsComponent::class, ['model' => $post])->assertSuccessful();
 });
 
-it('can mount the render the comments component for a model with comments', function() {
+it('can mount the render the comments component for a model with comments', function () {
     $comment = Comment::factory()->create();
 
     Livewire::test(CommentsComponent::class, ['model' => $comment->commentable])->assertSuccessful();
