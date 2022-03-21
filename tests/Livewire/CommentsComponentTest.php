d0ec2ed48529ec8080d9030096ca3f48ed91f059
Freek Van der Herten <freek@spatie.be>
2022-03-21 17:19:32 +0100

    wip

--- tests/Livewire/CommentsComponentTest.php
+++ tests/Livewire/CommentsComponentTest.php
@@ -6,8 +6,14 @@ use Spatie\LivewireComments\Livewire\CommentComponent;
 use Spatie\LivewireComments\Livewire\CommentsComponent;
 use Spatie\LivewireComments\Tests\Support\Models\Post;
 
-it('can mount the render the comments component', function() {
+it('can mount the render the comments component for a model without comments', function() {
     $post = Post::factory()->create();
 
     Livewire::test(CommentsComponent::class, ['model' => $post])->assertSuccessful();
+});
+
+it('can mount the render the comments component for a model with comments', function() {
+    $comment = Comment::factory()->create();
+
+    Livewire::test(CommentsComponent::class, ['model' => $comment->commentable])->assertSuccessful();
 });
