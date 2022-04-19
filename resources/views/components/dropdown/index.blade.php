8e40f1d46a4fe702866a4fc7469831f119b27a6b
Willem Van Bockstal <willem@spatie.be>
2022-04-19 20:24:54 +0200

    Reuse dropdown for subscriptions

--- resources/views/components/dropdown/index.blade.php
+++ resources/views/components/dropdown/index.blade.php
@@ -11,12 +11,16 @@
 >
      <button class="comments-dropdown-trigger" type="button" @click="dropdownOpen = !dropdownOpen">
           <span x-show="!dropdownOpen">
-               <x-comments::icons.menu />
-          </span>
-          <span x-show="dropdownOpen">
-               <x-comments::icons.close />
-          </span>
-    </button>
+          @isset ($trigger)
+               {{ $trigger }}
+          @else
+                    <x-comments::icons.menu />
+                    @endif
+               </span>
+               <span x-show="dropdownOpen">
+                    <x-comments::icons.close />
+               </span>
+     </button>
     <div x-show="dropdownOpen" class="comments-dropdown-items">
           {{ $slot }}
      </div>
