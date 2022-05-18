a9cb1d357d259d607cb6a39beb385aa82e8c5878
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2022-05-18 10:45:29 +0200

    CS

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -47,7 +47,7 @@
         @endforeach
         {{ $comments->links() }}
     @else
-        <p class="comment-no-comment-yet">{{ $this->noCommentsText ?? __('comments::comments.no_comments_yet') }}</p>
+        <p class="comments-no-comment-yet">{{ $this->noCommentsText ?? __('comments::comments.no_comments_yet') }}</p>
     @endif
 
     @if (! $this->newestFirst)
