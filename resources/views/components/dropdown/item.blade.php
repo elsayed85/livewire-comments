92364a05dbc4c259f4e013cabac0ce0b652ca5fc
Willem Van Bockstal <willem@spatie.be>
2022-05-24 21:31:06 +0200

    Replce dropdown by modal

--- resources/views/components/dropdown/item.blade.php
+++
@@ -1,6 +0,0 @@
-<button {{ $attributes->merge(['class' => 'comments-dropdown-item'])->except('icon') }}>
-    @isset($icon)
-        <x-dynamic-component :component="'comments::icons.' . $icon"/>
-    @endisset
-    {{ $slot }}
-</button>
