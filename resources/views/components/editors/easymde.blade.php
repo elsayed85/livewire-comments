39d674e4cd94a3c07c22e91ca8b1434b800af3db
Dan Palmieri <dlspalmieri@gmail.com>
2022-06-06 15:26:59 -0300

    fix

--- resources/views/components/editors/easymde.blade.php
+++ resources/views/components/editors/easymde.blade.php
@@ -12,10 +12,6 @@
     <div wire:ignore>
         <textarea placeholder="{{ $placeholder ?? '' }}"></textarea>
     </div>
-    
-    <div class="comments-form-editor-tip">
-        You can use <a href="https://spatie.be/markdown" target="_blank" rel="nofollow noopener noreferrer">Markdown</a>
-    </div>
 </div>
 
 @push('comments-scripts')
